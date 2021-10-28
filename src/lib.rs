dfb4ab61eefe1e9c4b8180ddded7b13c11c9c103
Emil Fresk <emil.fresk@gmail.com>
2021-10-28 11:43:37 +0200

    More tests (old not supported, that are now supported)

--- src/lib.rs
+++ src/lib.rs
@@ -653,12 +653,14 @@ mod test {
             Instant::<u32, 1, 1_000>::from_ticks(10) - Duration::<u32, 1, 1_000>::from_ticks(1);
         assert_eq!(diff, Instant::<u32, 1, 1_000>::from_ticks(9));
 
-        // // Instant - Duration, Different base (not supported)
-        // let sum: Instant<1, 10_000> = Instant::<1, 10_000>::from_ticks(10) + Duration::<1, 1_000>::from_ticks(1);
-        // assert_eq!(sum, Instant::<1, 10_000>::from_ticks(20));
+        // Instant - Duration, Different base
+        let sum: Instant<u32, 1, 10_000> =
+            Instant::<u32, 1, 10_000>::from_ticks(10) + Duration::<u32, 1, 1_000>::from_ticks(1);
+        assert_eq!(sum, Instant::<u32, 1, 10_000>::from_ticks(20));
 
-        // let diff: Instant<1, 10_000> = Instant::<1, 10_000>::from_ticks(10) - Duration::<1, 1_000>::from_ticks(1);
-        // assert_eq!(diff, Instant::<1, 10_000>::from_ticks(0));
+        let diff: Instant<u32, 1, 10_000> =
+            Instant::<u32, 1, 10_000>::from_ticks(10) - Duration::<u32, 1, 1_000>::from_ticks(1);
+        assert_eq!(diff, Instant::<u32, 1, 10_000>::from_ticks(0));
 
         // Instant + Extension trait
         let sum: Instant<u32, 1, 10_000> = Instant::<u32, 1, 10_000>::from_ticks(10) + 1.millis();
@@ -685,12 +687,14 @@ mod test {
             Instant::<u64, 1, 1_000>::from_ticks(10) - Duration::<u64, 1, 1_000>::from_ticks(1);
         assert_eq!(diff, Instant::<u64, 1, 1_000>::from_ticks(9));
 
-        // // Instant - Duration, Different base (not supported)
-        // let sum: Instant<1, 10_000> = Instant::<1, 10_000>::from_ticks(10) + Duration::<1, 1_000>::from_ticks(1);
-        // assert_eq!(sum, Instant::<1, 10_000>::from_ticks(20));
+        // Instant - Duration, Different base
+        let sum: Instant<u64, 1, 10_000> =
+            Instant::<u64, 1, 10_000>::from_ticks(10) + Duration::<u64, 1, 1_000>::from_ticks(1);
+        assert_eq!(sum, Instant::<u64, 1, 10_000>::from_ticks(20));
 
-        // let diff: Instant<1, 10_000> = Instant::<1, 10_000>::from_ticks(10) - Duration::<1, 1_000>::from_ticks(1);
-        // assert_eq!(diff, Instant::<1, 10_000>::from_ticks(0));
+        let diff: Instant<u64, 1, 10_000> =
+            Instant::<u64, 1, 10_000>::from_ticks(10) - Duration::<u64, 1, 1_000>::from_ticks(1);
+        assert_eq!(diff, Instant::<u64, 1, 10_000>::from_ticks(0));
 
         // Instant + Extension trait
         let sum: Instant<u64, 1, 10_000> = Instant::<u64, 1, 10_000>::from_ticks(10) + 1.millis();
